3c7c43945dc35c5e7344ec399493fe9e0f9907d5
lackingworth <worstface@yandex.ru>
2023-10-02 06:14:55 +0300

    Init

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+)
+
+const SIZE = 5
+
+type Node struct {
+	Val			string
+	Left		*Node
+	Right		*Node
+}
+
+type Queue struct {
+	Head		*Node
+	Tail		*Node
+	Length		int
+}
+
+type Cache struct {
+	Queue		Queue
+	Hash		Hash
+}
+
+type Hash map[string]*Node
+
+
+func (c *Cache) Check(str string) {
+	node := &Node{}
+
+	if val, ok := c.Hash[str]; ok {
+		node = c.Remove(val)
+	} else {
+		node = &Node{Val: str}
+	}
+	c.Add(node)
+	c.Hash[str] = node
+}
+
+func (c *Cache) Remove(n *Node) *Node {
+	fmt.Printf("Remove: %s\n", n.Val)
+	left := n.Left
+	right := n.Right
+
+	left.Right = right
+	right.Left = left
+	c.Queue.Length--
+	delete(c.Hash, n.Val)
+	
+	return n
+}
+
+func (c *Cache) Add(n *Node) {
+	fmt.Printf("Add: %s\n", n.Val)
+	tmp := c.Queue.Head.Right
+	
+	c.Queue.Head.Right = n 
+	n.Left = c.Queue.Head
+	n.Right = tmp
+	tmp.Left = n
+
+	c.Queue.Length++
+
+	if c.Queue.Length > SIZE {
+		c.Remove(c.Queue.Tail.Left)
+	}
+}
+
+func (c *Cache) Display() {
+	c.Queue.Display()
+}
+
+func (q *Queue) Display() {
+	node := q.Head.Right
+	fmt.Printf("%d - [", q.Length)
+
+	for i := 0; i < q.Length; i++ {
+		fmt.Printf("{%s}", node.Val)
+		if i < q.Length - 1 {
+			fmt.Printf("<-->")
+		}
+		node = node.Right
+	}
+	fmt.Println("]")
+}
+
+func NewCache() Cache {
+	return Cache{Queue: NewQueue(), Hash: Hash{}}
+}
+
+func NewQueue() Queue {
+	head := &Node{}
+	tail := &Node{}
+
+	head.Right = tail
+	tail.Left = head
+
+	return Queue{Head: head, Tail: tail}
+}
+
+func main() {
+	fmt.Println("START CACHE")
+	cache := NewCache()
+
+	for _, word := range []string{"house", "flat", "apartment", "shed", "tent", "penthouse", "villa"} {
+		cache.Check(word)
+		cache.Display()
+	}
+}
